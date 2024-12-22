0abde5f87e102fcde1daa9eec3d0ada8680b8f0d
hachimitsue <71762653+shinjitsue@users.noreply.github.com>
2024-12-22 09:56:39 +0800

    feat: 🎄 enhance AuthView and LoginForm components with improved layout and functionality

--- frontend/src/components/AuthView.vue
+++ frontend/src/components/AuthView.vue
@@ -4,25 +4,30 @@
   </div>
 
   <div class="login-container">
-    <v-row justify="center" align="center">
-      <v-col cols="10" lg="4" md="6" sm="6">
-        <login-form class="px-10">
-          <template #footer>
-            <v-card-text class="text-center mt-3 mb-5">
-              <span class="text-center font-weight-light">
-                Don't have an account?
-              </span>
-              <span
-                class="text-blue-lighten-2 text-decoration-none cursor-pointer"
-                @click="switchToRegister"
-              >
-                Register
-              </span>
-            </v-card-text>
-          </template>
-        </login-form>
-      </v-col>
-    </v-row>
+    <v-container>
+      <v-row justify="center" align="center">
+        <v-col cols="5" lg="5" md="5" class="px-8">
+          <login-form class="pa-10">
+            <template #footer>
+              <v-card-text class="text-center mt-3">
+                <span class="text-center font-weight-light">
+                  New on our platform?
+                </span>
+                <span
+                  class="text-blue-lighten-1 text-decoration-none cursor-pointer"
+                  @click="switchToRegister"
+                >
+                  Create an account
+                </span>
+              </v-card-text>
+            </template>
+          </login-form>
+        </v-col>
+        <v-col cols="5" md="5" lg="5">
+          <ChattingHubSvg />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 
   <v-dialog v-model="showRegister" persistent>
@@ -43,6 +48,7 @@ import { ref } from "vue";
 import LoginForm from "@/layouts/LoginForm.vue";
 import RegisterForm from "@/layouts/RegisterForm.vue";
 import BackgroundSvg from "@/components/svg/BackgroundSvg.vue";
+import ChattingHubSvg from "./svg/ChattingHubSvg.vue";
 
 const showRegister = ref(false);
 
